e669aa76405dade66b597a0d2554e06afc0e23c2
nachoperez714 <nachoperez714@gmail.com>
2020-08-22 01:44:09 -0300

    cada 1 hora

--- runSpace.py
+++ runSpace.py
@@ -9,6 +9,6 @@ while True:
 		reload(Space)#In case there were changes
 		seguir = Space.main(not(os.path.exists("sigo")))
 		open("sigo","w+")
-		time.sleep(60*30)
+		time.sleep(60*60)
 
 	os.remove("sigo")
