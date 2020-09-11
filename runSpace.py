e45f0d860b6474317121de7269205debc3eb500f
ferdr <ferdr@LAPTOP-4G5VHA51.localdomain>
2020-09-11 17:59:11 +0200

    Turn order and voting bugfix

--- runSpace.py
+++ runSpace.py
@@ -6,26 +6,18 @@ from importlib import reload
 while True:
 	seguir = True
 	while seguir:
+		reload(Space)#In case there were changes
 		if not(os.path.exists("sigo.txt")):
-			print("turno 0")
-			reload(Space)#In case there were changes
-			seguir = Space.main("0")
+			seguir = Space.main(0,vote=True)
 			f = open("sigo.txt","w+")
-			f.write(seguir)
+			f.write("1")
+			f.close()
 		else:
+			f = open("sigo.txt")
+			turno = int(f.read())
+			seguir = Space.main(turno,vote=True)
 			f = open("sigo.txt","w+")
-			turno = f.read()
-			print(turno)
-			if turno == "1":
-				print("turno 1")
-				reload(Space)#In case there were changes
-				seguir = Space.main(seguir)
-				f.write(seguir)
-			else:
-				reload(Space)#In case there were changes
-				seguir = Space.main("2")
-		if seguir == "0":
-			os.remove("sigo.txt")
-		time.sleep(60*60)
-
-	
+			f.write(str(turno+1))
+			f.close()
+		time.sleep(15)
+	os.remove("sigo.txt")	
