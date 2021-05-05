4fa3190f047f95bba2af1a4dafb869cf3df06141
FerDR <fer.delrio.445@gmail.com>
2021-05-05 08:33:02 +0200

    Fixed equipment bug

--- planets.py
+++ planets.py
@@ -1608,7 +1608,7 @@ class Equipment(Item):
 		if "on_lose" in self.properties[self.name]:
 			self.on_lose = self.properties[self.name]["on_lose"]
 
-	def Pass(self,spaceship):
+	def Pass(self,spaceship,event,was_portal,board):
 		pass
 
 	def give_luck(self,spaceship,amount=0):
